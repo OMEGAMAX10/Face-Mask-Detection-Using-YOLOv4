b5197aace0865fe0ab4aa94140ab715b07516cfd
Bogdan Marghescu <bogdan.marghescu99@gmail.com>
2020-12-08 08:35:12 +0200

    added dependencies script

---
+++ install_dependencies_ubuntu.sh
@@ -0,0 +1,30 @@
+cd ~
+apt-get update
+apt-get upgrade
+apt-get -y install build-essential cmake unzip pkg-config
+apt-get -y install libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran
+apt-get -y install python3-dev python3-numpy python3-pyqt5 pyqt5-dev-tools qttools5-dev-tools
+git clone https://github.com/opencv/opencv
+git clone https://github.com/opencv/opencv_contrib
+cd opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+	-D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D INSTALL_PYTHON_EXAMPLES=ON \
+	-D INSTALL_C_EXAMPLES=OFF \
+	-D OPENCV_ENABLE_NONFREE=ON \
+	-D WITH_CUDA=ON \
+	-D WITH_CUDNN=ON \
+	-D OPENCV_DNN_CUDA=ON \
+	-D ENABLE_FAST_MATH=1 \
+	-D CUDA_FAST_MATH=1 \
+	-D CUDA_ARCH_BIN=5.3 \
+	-D WITH_CUBLAS=1 \
+	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+	-D HAVE_opencv_python3=ON \
+	-D PYTHON_EXECUTABLE=/usr/bin/python3 \
+	-D BUILD_EXAMPLES=ON ..
+make
+make install
+ldconfig
